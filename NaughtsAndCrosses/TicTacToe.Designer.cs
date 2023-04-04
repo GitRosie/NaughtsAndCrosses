3486538ceca1ce681b515cdc889a78d199898bfb
Rosie <rosie_ledbrook@msn.com>
2023-04-04 16:11:48 +0100

    Add project files.

---
+++ NaughtsAndCrosses/TicTacToe.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace NaughtsAndCrosses
+{
+    partial class Form_TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Lbl_Player = new System.Windows.Forms.Label();
+            this.Btn_Player = new System.Windows.Forms.Button();
+            this.Btn_Restart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // Lbl_Player
+            // 
+            this.Lbl_Player.AutoSize = true;
+            this.Lbl_Player.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Player.Location = new System.Drawing.Point(475, 28);
+            this.Lbl_Player.Name = "Lbl_Player";
+            this.Lbl_Player.Size = new System.Drawing.Size(62, 24);
+            this.Lbl_Player.TabIndex = 1;
+            this.Lbl_Player.Text = "Player";
+            // 
+            // Btn_Player
+            // 
+            this.Btn_Player.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Player.Location = new System.Drawing.Point(479, 64);
+            this.Btn_Player.Name = "Btn_Player";
+            this.Btn_Player.Size = new System.Drawing.Size(109, 106);
+            this.Btn_Player.TabIndex = 2;
+            this.Btn_Player.Text = "X";
+            this.Btn_Player.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Restart
+            // 
+            this.Btn_Restart.Location = new System.Drawing.Point(479, 448);
+            this.Btn_Restart.Name = "Btn_Restart";
+            this.Btn_Restart.Size = new System.Drawing.Size(108, 27);
+            this.Btn_Restart.TabIndex = 3;
+            this.Btn_Restart.Text = "Restart";
+            this.Btn_Restart.UseVisualStyleBackColor = true;
+            this.Btn_Restart.Click += new System.EventHandler(this.Btn_Restart_Click);
+            // 
+            // Form_TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 498);
+            this.Controls.Add(this.Btn_Restart);
+            this.Controls.Add(this.Btn_Player);
+            this.Controls.Add(this.Lbl_Player);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form_TicTacToe";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Lbl_Player;
+        private System.Windows.Forms.Button Btn_Player;
+        private System.Windows.Forms.Button Btn_Restart;
+    }
+}
+
---
+++ NaughtsAndCrosses/TicTacToe.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NaughtsAndCrosses
+{
+    public partial class Form_TicTacToe : Form
+    {
+        public Form_TicTacToe()
+        {
+            InitializeComponent();
+            GenerateButtons();
+        }
+
+        Button[,] buttons = new Button[3,3];
+
+        private void GenerateButtons()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Size = new Size(150, 150);
+                    buttons[i, j].Location = new Point(i * 150, j * 150);
+                    buttons[i, j].FlatStyle = FlatStyle.Flat;
+                    buttons[i, j].Font = new Font(DefaultFont.FontFamily, 80, FontStyle.Bold);
+
+                    buttons[i, j].Click += new EventHandler(button_Click);
+
+                    panel1.Controls.Add(buttons[i, j]);
+                }
+            }
+        }
+
+        void button_Click(object sender, EventArgs e)
+        {
+            //load the clicked button into a local variable
+            Button button = sender as Button;
+            // Do nothing if button already clicked
+            if (button.Text != "")
+                return;
+            // Mark button with current player's icon
+            button.Text = Btn_Player.Text;
+            TogglePlayer();
+        }
+
+        private void TogglePlayer()
+        {
+            CheckIfGameEnds();
+
+            if (Btn_Player.Text == "X")
+                Btn_Player.Text = "O";
+            else
+                Btn_Player.Text = "X";
+        }
+
+        private void CheckIfGameEnds()
+        {
+            List<Button> winnerButtons = new List<Button>();
+
+            // Vertical
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (buttons[i, j].Text != Btn_Player.Text)
+                    {
+                        break;
+                    }
+
+                    winnerButtons.Add(buttons[i, j]);
+
+                    if (j == 2)
+                    {
+                        ShowWinner(winnerButtons);
+                        return;
+                    }
+                }
+            }
+
+            // Horizontal
+            for (int i = 0; i< 3; i++)
+            {
+                winnerButtons = new List<Button>();
+                for (int j = 0; j < 3; j++)
+                {
+                    if (buttons[j,i].Text != Btn_Player.Text)
+                    {
+                        break;
+                    }
+
+                    winnerButtons.Add(buttons[j, i]);
+                    if (j == 2)
+                    {
+                        ShowWinner(winnerButtons);
+                        return;
+                    }
+                }
+            }
+
+            // Diagonal top-left to bottom-right
+            winnerButtons = new List<Button>();
+            for (int i = 0, j = 0; i < 3; i++, j++)
+            {
+                if (buttons[i,j].Text != Btn_Player.Text)
+                {
+                    break;
+                }
+
+                winnerButtons.Add(buttons[i, j]);
+                if (j == 2)
+                {
+                    ShowWinner(winnerButtons);
+                    return;
+                }
+            }
+
+            // Diagonal bottom-left to top-right
+            winnerButtons = new List<Button>();
+            for (int i = 2, j = 0; j < 3; i--, j++)
+            {
+                if (buttons[i,j].Text != Btn_Player.Text)
+                { break; }
+
+                winnerButtons.Add(buttons[i, j]);
+                if (j == 2)
+                {
+                    ShowWinner(winnerButtons);
+                    return;
+                }
+            }
+
+            // All buttons clicked but no winner
+            foreach (var button in buttons)
+            {
+                if (button.Text == "")
+                    return;
+            }
+            MessageBox.Show("Game Draw", "Draw");
+        }
+
+        private void ShowWinner(List<Button> winnerButtons)
+        {
+            foreach (var button in winnerButtons)
+            {
+                button.BackColor = Color.Red;
+            }
+            MessageBox.Show("Player " + Btn_Player.Text + " wins!");
+        }
+
+        private void Btn_Restart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
